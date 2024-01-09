09e98a0f5546c03a729b8759a093df891f9062d5
MuaiadHadad <108369828+MuaiadHadad@users.noreply.github.com>
2024-01-09 18:54:17 +0000

    a

--- run.py
+++ run.py
@@ -2,13 +2,8 @@ from app.app import create_app
 import os
 
 app = create_app()
-
-# Verifica se estamos no ambiente de desenvolvimento local
 if __name__ == "__main__":
-    # Utiliza as variáveis de ambiente fornecidas pelo Vercel
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
-
-    # Inicia o aplicativo no servidor de desenvolvimento
     app.run(host=host, port=port)
 s
