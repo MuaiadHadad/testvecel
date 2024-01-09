42b8e9255ed77b79b4087816b62599cb08b4fb1c
MuaiadHadad <108369828+MuaiadHadad@users.noreply.github.com>
2024-01-09 18:58:06 +0000

    now

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-from app.app import create_app
-import os
 
+from app.app import create_app
 app = create_app()
+
 if __name__ == "__main__":
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host=host, port=port)
-s
+    app.run(host='192.168.1.242', port=5000)
