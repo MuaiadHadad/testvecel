a1d86f57f693f8dbc9ad882668e1f47a2717ddab
MuaiadHadad <108369828+MuaiadHadad@users.noreply.github.com>
2024-01-09 20:10:43 +0000

    now

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-# project/app/__init__.py
-from flask import Flask
-from .routes import create_routes
-
-
-def create_app():
-    app = Flask(__name__)
-    return app
--- run.py
+++
@@ -1,6 +0,0 @@
-
-from app.app import create_app
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='192.168.1.242', port=8000)
